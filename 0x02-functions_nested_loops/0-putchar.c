3241e630c98ba4bbbacbd78b51aabb11e5a5358e
Bobby88bob <robertwangechi@gmail.com>
2023-06-20 13:23:55 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putcher('c');
+	_putcher('h');
+	_putchar('a');
+	_putcher('r');
+	_putchar('\n');
+	return (0);
+}
+
